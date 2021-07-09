ca1159de4da75313eda38a37b68e0431c9a1f26d
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-07-09 14:05:34 +0600

    LoginActivity - boilerplate

---
+++ app/src/main/java/com/oho/oho/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.oho.oho.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.oho.oho.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
